95c9b8edb5006b8ec118d2c655c944427fb3e046
lucaslinyker <lucaslinyker@outlook.com>
2024-01-27 11:58:54 -0300

    :sparkles: feat: Adicionar formatação html

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import "./App.css"
+import "./App.css";
+import illustration from "./assets/images/illustration-article.svg";
+import avatar from "./assets/images/image-avatar.webp";
 
 function App() {
   return (
     <>
-      Learning
+      <main>
+        <img src={illustration} alt="Illustration" />
+        <h1>Learning</h1>
+        <p>Published 21 Dec 2023</p>
 
-      Published 21 Dec 2023
+        <h2>HTML & CSS foundations</h2>
+        <p>
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
 
-      HTML & CSS foundations
+        <img src={avatar} alt="Avatar of man" />
+        <h3>Greg Hooper</h3>
+      </main>
 
-      These languages are the backbone of every website, defining structure, content, and presentation.
-
-      Greg Hooper
-
-      <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
-      </div>
+      <footer className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/lucaslinyker">Lucas Linyker</a>.
+      </footer>
     </>
   );
 }
